67a1d6ea392a952acbe182f4e975408f1a2c4428
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2023-10-30 19:49:24 +0100

    again

--- src/pages/faq/faq.jsx
+++ src/pages/faq/faq.jsx
@@ -14,19 +14,23 @@ const Faq = () => {
         <p className='faq_sub'>Questions that might have an answer for you!</p>
       </div>
       <div className='blackbox1'>
+
+
+
+
+
         <div className='first_box'>
         <div className=""><p className='white_header'>General FAQ</p></div>
           <div className='white_box'>
           <div className="ci_header">What is CICATE?</div>
           <div>
 <p className="inside_text_para">
-CICATE (Competent International Test takers Assessment, Tests, and Exams) is a pioneering assessment and
-evaluation platform that assists individuals and organisations in identifying qualified Test takers for
-educational and professional opportunities internationally and abroad.
+CICATE (Competent International Test takers Assessment, <br /> Tests, and Exams) is a pioneering assessment and
+evaluation <br />platform that assists individuals and organisations in <br /> identifying qualified Test takers for
+educational and  <br />professional opportunities internationally and abroad.
 </p>
+</div> 
 </div>
-
-          </div>
           <div>
 <p><a href="/" className="inside_text">How does CICATE benefit Test takers? <img src={tick} alt="img" className='tick_image'/> </a></p>
 </div>
@@ -37,26 +41,29 @@ educational and professional opportunities internationally and abroad.
 <p><a href="/"className="inside_text">What are the key products of CICATE? <img src={tick} alt="img" className='tick_image'/></a></p>
 </div>
         </div>
+
+
+
+
+
         <div className='second_box'>
-        <div>
+       
 <p className="white_header">G.A.T.E. - Global <br /> Assessment Test <br />for Education FAQs</p>
-</div>
+
 <div className='inside_brown'>
-<p><a href="/" className="inside_text">What is G.A.T.E? <img src={tick} alt="img" className='tick_image'/> </a></p>
+<a href="/" className="inside_text">What is G.A.T.E? <img src={tick} alt="img" className='tick_image'/> </a>
 </div>
 <p><a href="/" className="inside_text">Who can take the G.A.T.E exam? <img src={tick} alt="img" className='tick_image'/> </a></p>
 <p><a href="/" className="inside_text"> How does G.A.T.E benefit Test takers?<img src={tick} alt="img" className='tick_image'/> </a></p>
-<p><a href="/" className="inside_text">Can G.A.T.E scores be used for multiple university applications? <img src={tick} alt="img" className='tick_image'/> </a></p>
 <p><a href="/" className="inside_text"> How is the G.A.T.E syllabus developed?<img src={tick} alt="img" className='tick_image'/> </a></p>
 <p><a href="/" className="inside_text"> What is the validity period of G.A.T.E scores?<img src={tick} alt="img" className='tick_image'/> </a></p>
-<p><a href="/" className="inside_text">Can G.A.T.E scores be used for scholarship applications? <img src={tick} alt="img" className='tick_image'/> </a></p>
 <p><a href="/" className="inside_text">How can I register for the G.A.T.E exam? <img src={tick} alt="img" className='tick_image'/> </a></p>
         </div>
       </div>
 
-      <div className='blackbox1'>
+      <div className='blackbox2'>
 
-   <div className='first_box1'>
+   <div className='first_box'>
    <div>
 <p className="white_header">P.O.C.A.S- Professional <br /> Assessment &amp; <br />Selection Suite FAQs</p>
 </div>
@@ -65,9 +72,8 @@ educational and professional opportunities internationally and abroad.
 <div>
 <p><a href="/" className="inside_text">Who can benefit from POCAS? <img src={tick} alt="img" className='tick_image'/> </a></p>
 <div>
-<p><a href="/" className="inside_text">Does POCAS cater to specific industries or roles? <img src={tick} alt="img" className='tick_image'/> </a></p>
+
 <div>
-<p><a href="/" className="inside_text">Does POCAS cater to specific industries or roles?<img src={tick} alt="img" className='tick_image'/> </a></p>
 <div>
 <p><a href="/" className="inside_text">How does CICATE benefit Test takers? <img src={tick} alt="img" className='tick_image'/> </a></p>
 <div>
