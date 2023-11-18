fe1fe6530bbf387ca21618a0f0288c0a84d2f72d
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2023-11-18 10:03:56 +0100

    push

--- src/pages/test_format/TestFormat.jsx
+++ src/pages/test_format/TestFormat.jsx
@@ -20,7 +20,7 @@ const TestFormat = () => {
   return (
     <div className='wrapper_main'>
       <div>
-      <Navbar/>
+    <Navbar/>
       </div>
       <div className='top_boxone'>
      <div className='new1'>
@@ -61,6 +61,7 @@ Reliable Tests</p>
     </div>
 
     <div className='top_bl_white'>
+
         <div className='bl_white'>
             <div className='inside_black_bl'></div>
             <p className='bl_tests'>G.A.T.E</p>
