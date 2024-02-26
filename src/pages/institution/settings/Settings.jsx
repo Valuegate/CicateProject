6b07ae0c5f4df9090c09cde3e36b69d149c0ac05
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2024-02-26 22:55:04 +0100

    final

--- src/pages/institution/settings/Settings.jsx
+++ src/pages/institution/settings/Settings.jsx
@@ -27,11 +27,8 @@ const SettingsUniversity = () => {
                <div className='top_profile'>
                <div className='insti-profile'>
                     <h1 className='profile_profile'>Institution Settings</h1>
-    
                     <div> <img src={guy3} alt="profile" /> </div>
-                    <div>
-                    <button className='profile_btn'>Upload</button>
-                    </div>
+                    <div><button className='profile_btn'>Upload</button></div>
                   
                 </div>
                </div>
