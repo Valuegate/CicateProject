12db4159b5460a4a70f17816dccd7d019161e8f2
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2024-02-26 17:04:47 +0100

    commit

--- src/pages/institution/settings/Settings.jsx
+++ src/pages/institution/settings/Settings.jsx
@@ -6,7 +6,7 @@ import cartgreen from '../../../assets/cartgreen.svg'
 import guy3 from '../../../assets/guy3.png'
 import IdSlide from '../../../components/containers/id_slide/IdSlide'
 import Footer from '../../../components/containers/footer/footer'
-
+import ResetPassword from '../../../ResetPassword'
 
 const SettingsUniversity = () => {
   return (
@@ -92,42 +92,8 @@ const SettingsUniversity = () => {
 
               
             </div>
-  
-            <div className='profile_justify'>
-<div className='profile_black4'>
-<div className='top_profile'>
-               <div className='profile_column'>
-                    <h1 className='profile_profile'>Password Settings</h1>
-                  
-                </div>
-               </div>
-               <form action="" className='form_profile'>
-                 <div className='profile_column'>
-                    <label htmlFor="" className=''> Username: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-
-                    <label htmlFor="" className=''> New Password: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-                 </div>
-                 <div className='profile_column'>
-                 <label htmlFor="" className=''> Existing Password: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-
-                    <label htmlFor="" className=''> Confirm Password: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-                 </div>
-                 
-               </form>
-               <div className='bottom_profile'>
-                <button className='profile_btn'>Save Changes</button>
-                <button className='profile_btn1'>Cancel</button>
-               </div>
-</div>
-</div>
+  <ResetPassword/>
+            
       </div>
   )
 }
--- src/pages/student/settings/Settings.jsx
+++ src/pages/student/settings/Settings.jsx
@@ -4,7 +4,7 @@ import cartgreen from '../../../assets/cartgreen.svg'
 import guy2 from '../../../assets/guy2.png'
 import IdSlide from '../../../components/containers/id_slide/IdSlide'
 import Footer from '../../../components/containers/footer/footer'
-
+import ResetPassword from '../../../ResetPassword'
 import './style.css'
 
 const Settings = () => {
@@ -65,43 +65,9 @@ const Settings = () => {
           
             </div>
   
-            <div className='profile_justify'>
-<div className='profile_black4'>
-<div className='top_profile'>
-               <div className='profile_column'>
-                    <h1 className='profile_profile'>Password Settings</h1>
-                  
-                </div>
-               </div>
-               <form action="" className='form_profile'>
-                 <div className='profile_column'>
-                    <label htmlFor="" className=''> Username: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-
-                    <label htmlFor="" className=''> New Password: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-                 </div>
-                 <div className='profile_column'>
-                 <label htmlFor="" className=''> Existing Password: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-
-                    <label htmlFor="" className=''> Confirm Password: <br />
-                        <input type="text" className='inputs'/>
-                    </label>
-                 </div>
-                 
-               </form>
-               <div className='bottom_profile'>
-                <button className='profile_btn'>Save Changes</button>
-                <button className='profile_btn1'>Cancel</button>
-               </div>
-</div>
-
+<ResetPassword/>
           </div>
-      </div>
+   
     
   )
 }
