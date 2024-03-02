dca4a985509a4719d532f646ca817ebcc8d2603e
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2024-03-02 09:18:23 +0100

    testing

--- src/pages/test_format/TestFormat.jsx
+++ src/pages/test_format/TestFormat.jsx
@@ -48,7 +48,7 @@ const [user, setUser] = useState('')
 
       <div className='top_abiities'>
     
-        <div className='abilities'>Test Your Abilities <br />
+        <div className='abilities'>Test Your good Abilities <br />
 to Success!</div>
       </div>
 
