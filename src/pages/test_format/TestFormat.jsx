b8f19a14000eae7373e925fa95a0a89a44164440
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2024-03-04 17:24:32 +0100

    push tets

--- src/pages/test_format/TestFormat.jsx
+++ src/pages/test_format/TestFormat.jsx
@@ -48,7 +48,7 @@ const [user, setUser] = useState('')
 
       <div className='top_abiities'>
     
-        <div className='abilities'>Testing1 Your Abilities but bad<br />
+        <div className='abilities'>Testing Your Abilities<br />
 to Success!</div>
       </div>
 
