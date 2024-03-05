2fc66452fa0df8d9e655b2a08c02e2f01283fa32
Sesugh Asa <itsmason58@gmail.com>
2024-03-05 17:29:09 +0100

    sweety

--- src/pages/test_format/TestFormat.jsx
+++ src/pages/test_format/TestFormat.jsx
@@ -48,7 +48,7 @@ const [user, setUser] = useState('')
 
       <div className='top_abiities'>
     
-        <div className='abilities'>Testing Your Abilities <br />
+        <div className='abilities'>Test Your Abilities <br />
 to Success!</div>
       </div>
 
