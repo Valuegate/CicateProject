1345333397a5b4d1a4d45a288784d8c52ddc4fca
Sesugh Asa <itsmason58@gmail.com>
2024-03-25 07:57:51 +0100

    again

--- src/App.js
+++ src/App.js
@@ -66,7 +66,9 @@ function App() {
 <Router>
      <RegisterContext>
       <AuthLogin>
-
+<ProtectedRoutes>
+  
+</ProtectedRoutes>
      
       <Routes>
       <Route path='/student/testresult' element={<TestResult/>}/>
